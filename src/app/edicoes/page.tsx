2bba30f8f0d75f64e34fee3b70ffdba8f3eb98fe
Wesley Rocha <wesleysr1997@gmail.com>
2024-07-12 07:44:59 -0400

    Add FAB to post editions

--- src/app/edicoes/page.tsx
+++ src/app/edicoes/page.tsx
@@ -1,4 +1,4 @@
-import { getEditions, getMatches } from "@/lib/api";
+import { getEditions } from "@/lib/api";
 import { DataTable } from "../../components/data-table";
 import { columns } from "./columns";
 import {
@@ -7,11 +7,15 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { FloatingActionButton } from "@/components/floating-action-button";
 
 export default async function Page() {
   const editions = await getEditions();
   return (
     <main className="px-4 md:px-6 flex flex-col gap-y-4">
+      <div className="fixed md:hidden flex">
+        <FloatingActionButton />
+      </div>
       <Card>
         <CardHeader>
           <CardTitle>Edições</CardTitle>
@@ -20,7 +24,15 @@ export default async function Page() {
           </CardDescription>
         </CardHeader>
       </Card>
-      <DataTable columns={columns} data={editions} />
+      <DataTable
+        columns={columns}
+        data={editions}
+        initialState={{
+          pagination: {
+            pageSize: 200,
+          },
+        }}
+      />
     </main>
   );
 }
