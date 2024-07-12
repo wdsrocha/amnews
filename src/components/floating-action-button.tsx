fb915abf87354006f5692b795e19ac8349b96c2b
Wesley Rocha <wesleysr1997@gmail.com>
2024-07-12 08:02:42 -0400

    Fix floating action button being under the table
    The fix for this is a potential bug for the future, as I had to remove position
    relative to table shadcn component. Why was it there, in the first place?

--- src/components/floating-action-button.tsx
+++ src/components/floating-action-button.tsx
@@ -6,8 +6,7 @@ import { Button } from "./ui/button";
 export const FloatingActionButton = () => {
   return (
     <Button
-      size="icon"
-      className="fixed z-10 bottom-4 right-4 h-14 w-14 rounded-full shadow-2xl"
+      className="fixed z-10 bottom-3 right-3 h-14 w-14 rounded-full shadow-2xl"
       onClick={() => console.log("click")}
     >
       <PlusIcon className="h-5 w-5" />
